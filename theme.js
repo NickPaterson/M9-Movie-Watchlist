b40ff8e54d66aee545be732289a9aa1fa1b2a512
Nick <Npgb18@gmail.com>
2022-08-13 09:33:30 +0100

    theme added to localStorage

--- theme.js
+++ theme.js
@@ -1,10 +1,21 @@
 
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]')
+// if local storage theme is dark apply dark theme
+if (localStorage.getItem('theme') === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    toggleSwitch.checked = true
+}
 
 function switchTheme(e) {
-    (e.target.checked) 
-        ? document.documentElement.setAttribute('data-theme', 'dark')
-        : document.documentElement.setAttribute('data-theme', 'light')   
+    // set local storage
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark')
+        localStorage.setItem('theme', 'dark')
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light')
+        localStorage.setItem('theme', 'light')
+    }   
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false)
+
